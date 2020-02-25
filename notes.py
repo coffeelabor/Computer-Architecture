b2739e42c61e4b8b516bfcf6f8b6a8a406f822e5
Reed James <reedtjames1234@gmail.com>
2020-02-25 17:57:56 -0600

    initial commit

---
+++ notes.py
@@ -0,0 +1,45 @@
+'''
+############### Step 0 #######################
+'''
+Make a list of files here.
+    asm:
+        asm.js DEPRICATED
+        asm.py ?
+        buildall 
+        call.asm
+        interrups.asm
+        keyboard.asm
+        mult.asm
+        print8.asm
+        sctest.asm
+        stack.asm
+        stackoverflow.asm
+    ls8:
+        examples:
+            call.ls8
+            interrupts.ls8
+            keyboard.ls8
+            mult.ls8
+            print8.ls8
+            printstr.ls8
+            sctest.ls8
+            stack.ls8
+            stackoverflow.ls8
+        cpu.py
+        ls8.py
+
+'''
+############### Step 0 #######################
+'''
+'''
+############### Step 1 #######################
+'''
+'''
+############### Step 1 #######################
+'''
+'''
+############### Step 1 #######################
+'''
+'''
+############### Step 1 #######################
+'''
